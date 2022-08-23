fdd87b12e71c36a778926538a05a509f9a48f2e0
Steven <aofldjxm@gmail.com>
2022-08-23 18:16:09 +0900

    Update: Get param and body

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,18 +1,29 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, Res } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './create-cat-dto';
+import { Request, Response } from 'express';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
-  findAll() {
-    return this.catsService.findAll();
+  findAll(@Req() req: Request, @Res() res: Response) {
+    const data = this.catsService.findAll();
+    res.json(data);
+  }
+
+  @Get('/:id')
+  findOne(@Param('id') id: string) {
+    console.log(id);
   }
 
   @Post()
-  create(@Body() createCatDto: CreateCatDto) {
+  create(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Body() createCatDto: CreateCatDto,
+  ) {
     /*
       {
         name: string,
